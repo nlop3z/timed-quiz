060d4f32fb69ecd93d1897507c95266ecb301b3f
nlopez <nelly.lopez@utexas.edu>
2020-10-19 18:58:21 -0500

    cleaned up code and fixed final score problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,54 +15,50 @@ var choicesEl = document.getElementById('choices');
 var initialsEl = document.getElementById('initials');
 var finalScoreEl = document.getElementById('final-score');
 var goBackButton = document.getElementById('back');
-
-
-//array of quiz questions
+// Array of quiz questions
 const questions = [
     {
         questionTitle: "Commonly used data types DO NOT include:",
-        answers: ["1. strings","2. booleans","3. alerts","4. numbers"],
+        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
         correctAnswer: "3. alerts"
     },
     {
         questionTitle: "The condition in an if / else statement is enclosed with ____",
-        answers: ["1. quotes","2. curly brackets","3. parenthesis","4. square brackets"],
+        answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
         correctAnswer: "3. parenthesis"
     },
     {
         questionTitle: "Arrays in JavaScript can be used to store ____",
-        answers: ["1. numbers and strings", "2. other arrays","3. booleans","4. all of the above"],
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
         correctAnswer: "4. all of the above"
     },
     {
         questionTitle: "String values must be enclosed within ____ when being assigned to variables.",
-        answers: ["1. commas","2. curlybrackets","3. quotes","4. parenthesis"],
+        answers: ["1. commas", "2. curlybrackets", "3. quotes", "4. parenthesis"],
         correctAnswer: "4. parenthesis"
     },
     {
         questionTitle: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["1. JavaScript","2. terminal/bash","3. for loops","4. console.log"],
+        answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
         correctAnswer: "4. console.log"
     }
 ];
-
-//Timer that counts down
+// Timer
 var counter = 5;
-var countdown = function() {
+var countdown = function () {
     if (time <= 0 || questionCounter > 4) {
-        
+
         endQuiz();
     } else {
         time--;
         timerEl.textContent = time;
     }
 }
-
 var startQuiz = function () {
     startCountdown = setInterval(countdown, 1000);
     grabQuestion();
 }
-
+// Fx that will grab the questions and put them on the screen
 var grabQuestion = function () {
     document.getElementById("start-page").classList.add("hidden")
     document.getElementById("high-score").classList.add("hidden")
@@ -70,52 +66,44 @@ var grabQuestion = function () {
     var currentQuestion = questions[questionCounter];
     questionTitleEl.textContent = currentQuestion.questionTitle;
     choicesEl.textContent = '';
-    //console.log(currentQuestion.answers.length);
     for (var i = 0; i < currentQuestion.answers.length; i++) {
         var choicesButton = document.createElement("button")
         choicesButton.textContent = currentQuestion.answers[i]
         choicesButton.setAttribute("value", currentQuestion.answers[i])
-        choicesButton.onclick=answerCheck
+        choicesButton.onclick = answerCheck
         choicesEl.appendChild(choicesButton)
-        choicesButton.classList.add ("show","auto");
-        //console.log(choicesButton);
-
+        choicesButton.classList.add("show", "auto");
     }
 }
-
-//answer check fx
+// Answer check fx
 var answerCheck = function () {
     var currentQuestion = questions[questionCounter].correctAnswer;
     var guessAnswer = this.value;
-    //console.log(guessAnswer)
     document.getElementById("feedback").classList.remove("hidden")
     if (currentQuestion === guessAnswer) {
         document.getElementById("feedback").textContent = "correct"
     } else {
         document.getElementById("feedback").textContent = "wrong"
-        time=time-10;
-    } 
-    if (questionCounter===questions.length - 1){
-    endQuiz();
-    finalScoreEl.textContent = time;
-    return;
-}
+        time = time - 10;
+    }
+    if (questionCounter === questions.length - 1) {
+        endQuiz();
+        finalScoreEl.textContent = time;
+        return;
+    }
     questionCounter++
     grabQuestion();
 }
-
-//end quiz fx
-var endQuiz = function (){
+// End quiz fx
+var endQuiz = function () {
     clearInterval(startCountdown);
     document.getElementById("end-page").classList.remove("hidden")
     document.getElementById("feedback").classList.add("hidden")
     document.getElementById("questions-container").classList.add("hidden")
     document.getElementById("high-score").classList.add("hidden")
-
+    timerEl.textContent = time;
 }
-
-
-//fx to save score and enter initials
+// Fx to save score and enter initials
 var saveHighScore = function () {
     var enterInitials = initialsEl.value.trim();
     if (initials !== '') {
@@ -125,48 +113,45 @@ var saveHighScore = function () {
             score: time
         };
         highscores.push(newScore);
-        highscores.sort(function(a,b){return b.score-a.score})
+        highscores.sort(function (a, b) { return b.score - a.score })
         localStorage.setItem('scores', JSON.stringify(highscores));
     }
-    //finalScoreEl.textContent = newScore.time;
     getGame(highscores);
 };
-
-//restart game and get scores fx
-var getGame = function(storageScores) {
+// Get scores fx
+var getGame = function (storageScores) {
     document.getElementById("high-score").classList.remove("hidden")
     document.getElementById("end-page").classList.add("hidden")
     var backButton = document.getElementById('back');
     backButton = document.createElement("button");
     document.getElementById("list").classList.remove("hidden")
     var list = document.getElementById("list")
-    for (let index=0; index < highscores.length && index < 3; index++){
+    for (let index = 0; index < highscores.length && index < 3; index++) {
         var scoreEl = document.createElement("li");
-        scoreEl.innerHTML="initials: " + storageScores[index].initials + " score: " + storageScores[index].score
+        scoreEl.innerHTML = "initials: " + storageScores[index].initials + " score: " + storageScores[index].score
         document.getElementById("list").append(scoreEl);
     }
     clearInterval();
 }
-
-var clearScores = function() {
+// Clear scores 
+var clearScores = function () {
     localStorage.removeItem('scores');
-    document.getElementById("list").innerHTML = "";    
+    document.getElementById("list").innerHTML = "";
 }
-
+// Reset quiz to start it over
 var resetQuiz = function () {
     document.getElementById("end-page").classList.add("hidden")
     document.getElementById("feedback").classList.add("hidden")
     document.getElementById("questions-container").classList.add("hidden")
     document.getElementById("high-score").classList.add("hidden")
     document.getElementById("start-page").classList.remove("hidden")
-    document.getElementById("list").innerHTML = ""; 
+    document.getElementById("list").innerHTML = "";
     questionCounter = 0;
     time = 75;
     timerEl.textContent = 0;
-    clearInterval();    
+    clearInterval();
 }
-
-//Listeners
+// Listeners
 startButton.addEventListener("click", startQuiz);
 submitButton.addEventListener("click", saveHighScore);
 clearButton.addEventListener("click", clearScores);
